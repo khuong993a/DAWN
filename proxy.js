6cd6d20a96003bc5d1c9add0825f059561362068
khuong993a <hoangsnew@gmail.com>
2024-11-15 10:03:56 +0700

    Create proxy.js

---
+++ proxy.js
@@ -0,0 +1,6 @@
+module.exports = [
+    'socks5://proxy1:port',
+    'socks5://proxy2:port',
+    'socks5://user:pass@proxy:port',
+    // Replace these with your actual SOCKS5 proxies if needed
+];
